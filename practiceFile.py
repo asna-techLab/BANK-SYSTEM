1bdbc57cf09142aaa9ad03278d52e8f7ff6601fe
asna-techLab <su92-bscsm-f24-490@superior.edu.pk>
2026-08-12 13:00:22 +0500

    sum of numbers

--- practiceFile.py
+++ practiceFile.py
@@ -1,7 +1,15 @@
 # ---practice Q1----
 
-person_name=input("enter the name of hero:")
-age=53
-height=1.83
+#person_name=input("enter the name of hero:")
+#age=53
+#height=1.83
 
-print (person_name)
+#print (person_name)
+
+#---sum of two numbers----
+
+a=int(input("enter 1st number:"))
+b=int(input("enter 2st number:"))
+
+print(a+b)
+
