d3831b89a390f51b38d264108830d3a1e6f9b061
asna-techLab <su92-bscsm-f24-490@superior.edu.pk>
2026-08-12 14:29:01 +0500

    MINI Calculator

--- practiceFile.py
+++ practiceFile.py
@@ -8,8 +8,46 @@
 
 #---sum of two numbers----
 
-a=int(input("enter 1st number:"))
-b=int(input("enter 2st number:"))
+#a=int(input("enter 1st number:"))
+#b=int(input("enter 2st number:"))
 
-print(a+b)
+#print("sum:" ,a+b)
 
+#---- practice Q2 ------
+#a =int(input("enter price of 1st product"))
+#b =int(input("enter price of 2st product"))
+#c =int(input("enter price of 3st product"))
+
+#Total= a+b+c
+#print ("total bill:",Total)
+
+#Avg = (a+b+c)/3
+#print ("average:",Avg)
+
+#person_name=input("enter the name of hero:") 
+#print (person_name)
+#print('o' in person_name)
+
+#str1=4>9
+#print(str1)
+
+#------- MINI CALCULATOR --------
+
+a=int(input("enter first digit:"))
+b=int(input("enter second digit:"))
+operator=input("enter operator:")
+
+if operator=='+':
+    print(a+b)
+elif operator=='-':
+    print(a-b)
+elif operator=='*':
+    print(a*b)
+elif operator=='/':
+    print(a/b)
+elif operator=='%':
+    print(a%b)
+elif operator=='**':
+    print(a**b)  
+else :
+    print("invalid opeartor")
